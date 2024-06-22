25b4582b4cb527df80a21c7c472c5055bad2e3cc
MikeB1124 <56797038+MikeB1124@users.noreply.github.com>
2024-06-22 15:07:54 -0700

    off load secrets manager function to utils package

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -1,15 +1,11 @@
 package configuration
 
 import (
-	"context"
 	"encoding/json"
 	"log"
 	"os"
 
 	stockslambdautils "github.com/MikeB1124/stocks-lambda-utils/v2"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 )
 
 type Configration struct {
@@ -39,7 +35,7 @@ func init() {
 		log.Fatal("SHARED_SECRETS environment variable is required")
 	}
 
-	secrets, err := getSecrets(sharedSecretName)
+	secrets, err := stockslambdautils.AWSGetSecrets(sharedSecretName)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -56,28 +52,6 @@ func init() {
 	}
 	AlpacaClient = stockslambdautils.NewAlpacaClient(lambdaConfig.Alpaca.ApiKey, lambdaConfig.Alpaca.ApiSecret, lambdaConfig.Alpaca.PaperApiUrl)
 	Config = lambdaConfig
-}
-
-func getSecrets(secretName string) (string, error) {
-	config, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Create Secrets Manager client
-	svc := secretsmanager.NewFromConfig(config)
-
-	input := &secretsmanager.GetSecretValueInput{
-		SecretId:     aws.String(secretName),
-		VersionStage: aws.String("AWSCURRENT"),
-	}
-
-	result, err := svc.GetSecretValue(context.TODO(), input)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	return *result.SecretString, nil
 }
 
 func GetConfig() Configration {
