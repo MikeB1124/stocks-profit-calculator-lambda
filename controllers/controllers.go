71fb49e8f8ce1231427e8617dda7cfa54986a581
MikeB1124 <56797038+MikeB1124@users.noreply.github.com>
2024-06-22 13:30:32 -0700

    using stocks lambda utils package

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,30 +4,26 @@ import (
 	"context"
 	"log"
 
-	"github.com/MikeB1124/stocks-profit-calculator-lambda/db"
+	stockslambdautils "github.com/MikeB1124/stocks-lambda-utils"
+	"github.com/MikeB1124/stocks-profit-calculator-lambda/clients"
 	"github.com/aws/aws-lambda-go/events"
 )
 
-type Response struct {
-	StatusCode int    `json:"statusCode"`
-	Message    string `json:"message"`
-}
-
 func CalulateTradeProfits(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// Complete Trade For Expired or Cancelled Orders
-	expireUpdateResult, err := db.UpdateAllExpiredOrders()
+	expireUpdateResult, err := clients.MongoClient.UpdateAllExpiredOrders()
 	if err != nil {
 		log.Printf("Error updating expired orders: %s", err.Error())
-		return createResponse(Response{Message: err.Error(), StatusCode: 500})
+		return stockslambdautils.CreateResponse(stockslambdautils.Response{Message: err.Error(), StatusCode: 500})
 	}
 	log.Printf("Updated %d expired orders", expireUpdateResult.ModifiedCount)
 
-	cancelUpdateResult, err := db.UpdateAllCancelledOrders()
+	cancelUpdateResult, err := clients.MongoClient.UpdateAllCancelledOrders()
 	if err != nil {
 		log.Printf("Error updating cancelled orders: %s", err.Error())
-		return createResponse(Response{Message: err.Error(), StatusCode: 500})
+		return stockslambdautils.CreateResponse(stockslambdautils.Response{Message: err.Error(), StatusCode: 500})
 	}
 	log.Printf("Updated %d cancelled orders", cancelUpdateResult.ModifiedCount)
 
-	return createResponse(Response{Message: "OK", StatusCode: 200})
+	return stockslambdautils.CreateResponse(stockslambdautils.Response{Message: "OK", StatusCode: 200})
 }
